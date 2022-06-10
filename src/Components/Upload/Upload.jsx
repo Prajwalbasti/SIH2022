162306a88efd9941bc1dfdd424875b18ec74c756
AnandD17 <avdodamani123@gmail.com>
2022-06-10 20:07:46 +0530

    added progress bar along with the state✅

--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -1,68 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState , useEffect } from 'react'
 import './Upload.scss'
 import logo from './../../Assets/uploadLogo.svg'
 import FileShow from '../FileShow/FileShow';
 
 
-function Upload({setData}) {
-  
+function Upload({ setData }) {
+
 
 
   const [file, setFile] = useState();
   const [fileName, setFileName] = useState();
 
   const [uploaded, setUploaded] = useState(false);
-  
+
+  const [progress,setProgress] = useState('0%')
+
+  const [progressState,setProgressState] = useState('Upload File')
+
   const handleUpload = (e) => {
     setFile(e.target.files);
     setUploaded(true);
   }
 
 
+  const progreeHandler = ()=>{
+    setTimeout(() => {
+      setProgressState('Unzipping the file');
+      setProgress('15%');
+    }, 500);
+    setTimeout(() => {
+      setProgressState('processing the file');
+      setProgress('30%');
+    }, 1000);
+    setTimeout(() => {
+      setProgressState('detecting duplicates');
+      setProgress('40%');
+    }, 1500);
+    setTimeout(() => {
+      setProgressState('Aligning contents');
+      setProgress('55%');
+    }, 2000);
+    setTimeout(() => {
+      setProgressState('Re-evaluating everything');
+      setProgress('70%');
+    }, 2500);
+    setTimeout(() => {
+      setProgressState('getting things done');
+      setProgress('85%');
+    }, 3000);
+    setTimeout(() => {
+      setProgressState('file is ready');
+      setProgress('100%');
+    }, 3500);
+  }
+
+useEffect(() => {
+  progreeHandler();
+}, [])
 
 
-  
+
 
   return (
     <div className='upload'>
 
       {uploaded ? null :
-      
-      <div className="bg">
-            <div className="uploader">
-              <div className='upload-drop'>
-                <div className="upload-inner">
-                  <div className='upload-img'>
-                    <img src={logo} alt="logo" />
-                    <input
-  type="file"
-  hidden=""
-  accept=".xlsx,.xls"
-  id="input1"
-  multiple
-  onChange={(e) => handleUpload(e)}
 
-/>
+        <div className="bg">
+          <div className="uploader">
+            <div className='upload-drop'>
+              <div className="upload-inner">
+                <div className='upload-img'>
+                  <img src={logo} alt="logo" />
+                  <input
+                    type="file"
+                    hidden=""
+                    accept=".xlsx,.xls"
+                    id="input1"
+                    multiple
+                    onChange={(e) => handleUpload(e)}
 
-                  </div>
-                  
-                  <div className='upload-txt'>
-                    {fileName ? 
-                    <p>{fileName}</p> :<p> Upload File</p> }</div>
+                  />
+
                 </div>
-              </div>
-              <div className='upload-loader'>
+
+                <div className='upload-txt'>
+                  {fileName ?
+                    <p>{fileName}</p> : <p>{progressState}</p>}</div>
               </div>
             </div>
+            <div className='upload-loader'>
+              <div className="progress-bar" style={{width:progress}}>
+
+              </div>
+            </div>
+          </div>
         </div>
 
       }
-        
 
-{uploaded ? <div className="uploaded">
-          <FileShow setData={setData} file={file} />
-        </div> : null }
-        
+
+      {uploaded ? <div className="uploaded">
+        <FileShow setData={setData} file={file} />
+      </div> : null}
+
     </div>
   )
 }
