ad564721b2bee9b05c650675f606187aa02d5668
Prajwalbasti59 <prajwalbasti59@gmail.com>
2022-06-24 17:11:01 +0530

    master rate list from firebase

--- src/Pages/MasterList/MasterList.jsx
+++ src/Pages/MasterList/MasterList.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
-
+import { db, auth } from "../../firebase/firebase";
 import {masterData} from "../../Utils/data.js"
+import { useState, useEffect } from "react";
+import firebase from "firebase";
+
 import "./MasterList.scss"
 
-const headers = ["Provider Name",  "Item Description 2", "Rate"]
+const headers = ["Provider Name", "Location ",  "Item Description ", "Item Amount"]
+ 
 
 function MasterList() {
+    const [info, setInfo] = useState(null);
+   
+    const doc =  db
+           .collection("Master Rate List")     
+           .doc("q4AOA8x0RrQBdYQbvmCH")  
+           .get().then((doc) => {
+                
+               setInfo(doc.data());
+               
+               
+               for (const key in doc.data()) {
+                console.log(key);
+                for(const value in doc.data()[key]){
+                    console.log(`${value}`);
+                    for (const i in doc.data()[key][value])
+                    {
+                        console.log(`${i} : ${doc.data()[key][value][i]}`)
+                    }
+                   
+                }
+            }
+           
+              }
+              
+              );
   return (
     <div className='master-list'>
+        
         <table className="table">
             <thead>
                 <tr>
@@ -18,17 +48,19 @@ function MasterList() {
                 </tr>
             </thead>
             <tbody>
+
                 {masterData && masterData.length != 0 ? masterData.map((dat, key) => {
                     
                     return <tr className={key%2 != 0 ? "color" : "non-color"} key={key}>
                          <td><input type="text" defaultValue={dat["Provider Name"]}/></td>
-                         {/* <td><input type="text" defaultValue={dat.Location}/></td> */}
+                        
                          <td><input type="text" defaultValue={dat.ItemDescription2}/></td>
                          <td><input type="text" defaultValue={dat.Rate}/></td>
                     </tr>
                     
                     
                 }) : null}
+
             </tbody>
         </table>
     </div>
