e6dd3f12904c6551c273bca5e0f6532e216437a3
Prajwalbasti59 <prajwalbasti59@gmail.com>
2022-06-25 19:48:43 +0530

    Masterlist from firebase

--- src/Pages/MasterList/MasterList.jsx
+++ src/Pages/MasterList/MasterList.jsx
@@ -1,56 +1,47 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
 import { db, auth } from "../../firebase/firebase";
-import {masterData} from "../../Utils/data.js"
+import { masterData } from "../../Utils/data.js"
 import { useState, useEffect } from "react";
 import firebase from "firebase";
 
 import "./MasterList.scss"
 
-const headers = ["Provider Name", "Location ",  "Item Description ", "Item Amount"]
- 
+const headers = ["Provider Name", "Rate "]
+
 
 function MasterList() {
     const [info, setInfo] = useState(null);
-   
+
     const [edit, setEdit] = useState(false);
 
-    const doc = () => { db
-           .collection("Master Rate List")     
-           .doc("q4AOA8x0RrQBdYQbvmCH")  
-           .get().then((doc) => { 
-               setInfo(doc.data());
-               
-               const data = [];
-
-               for (const key in doc.data()) {
-                console.log(key);
-                for(const value in doc.data()[key]){
-                    console.log(`${value}`);
-                    data.push(doc.data()[key][value]);
-                    // for (const i in doc.data()[key][value])
-                    // {
-                    //     data1.push(doc.data()[key][value][i])                        
-                    //     // console.log(`${i} : ${doc.data()[key][value][i]}`)
-                    //
-                }
-            }
-        setInfo(data);
-        });
-            }
+    const ref = firebase.firestore().collection("Master List")
+    const [data, setdata] = useState([])
+    const [loader, setloader] = useState(true)
+   
+    function getData() {
+        ref.onSnapshot((querySnapshot) => {
+            const items = []
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data())
+            })
+            setdata(items)
+            setloader(false)
+        })
+    }
 
     useEffect(() => {
-                doc()
-            }, [])
+        getData()
+    }, [])
+   
 
-
-  return (
-    <div className='master-list'>
+    return (
+        <div className='master-list'>
+           
+            <div className="button_div">
+        {edit ? <button onClick={()=> setEdit(false)}>Save</button> : <button onClick={()=> setEdit(true)}>Edit</button> }
         
-        <div className="button_div">
-        {edit ? <button >Save</button> : <button onClick={()=> setEdit(true)}>Edit</button> }
-        
-        <button>ADD PROVIDER</button>
+        <button >ADD PROVIDER</button>
 
         </div>
         <table className="table">
@@ -63,25 +54,25 @@ function MasterList() {
             </thead>
             <tbody>
 
-                { masterData && masterData.length != 0 ? masterData.map((dat, key) => {
+                { loader===false && (data.map((invoices, key) => {
                   return <tr className={key%2 != 0 ? "color" : "non-color"} key={key}>
-                  <td>{edit ? <input type="text" defaultValue={dat["Provider Name"]}/> :  <p>{dat["Provider Name"]}</p>}</td>
-                 
-                <td>{edit ? <input type="text" defaultValue={dat.ItemDescription2}/> : <p>{dat.ItemDescription2}</p>}</td>
-                  <td>{edit ? <input type="text" defaultValue={dat.Rate}/> : <p>{dat.Rate}</p> }</td>
+                  <td>{edit ? <input type="text" defaultValue={invoices["Provider Name"]}/> :  <p>{invoices["Provider Name"]}</p>}</td>
+                  <td>{edit ? <input type="text" defaultValue={invoices.Rate}/> : <p>{invoices.Rate}</p> }</td>
              </tr>
            
                         
                     
-                }) : null  }
+                }))   
+                }
 
                 
 
 
             </tbody>
         </table>
-    </div>
-  )
+
+        </div>
+    );
 }
 
 export default MasterList
